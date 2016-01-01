f7a44e7ccb293da1972989e417750523273497c1
Dad <Dad@Office-ENVY.home>
2016-01-01 17:09:06 -0500

    Renamed class

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Integer_Test {
+public class MyInteger_Test {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
